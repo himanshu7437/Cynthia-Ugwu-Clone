2250f23dc15ab109113bac9be1cf16e70b3db0e3
himanshu7437 <himanshujangra368@gmail.com>
2025-08-14 09:18:51 +0530

    first page animations done

--- script.js
+++ script.js
@@ -3,14 +3,39 @@ const scroll = new LocomotiveScroll({
     smooth: true
 });
 
-var crsr = document.querySelector('.crsr');
-var maindiv = document.querySelector('.main');
-
-
 function crsrfollower() {
+    var crsr = document.querySelector('.crsr');
     window.addEventListener('mousemove', function(dets) {
         crsr.style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-    }) 
+    })
+}
+
+function firstPage() {
+    var tl = gsap.timeline();
+
+    tl.from(".nav", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.5,
+        ease: Expo.easeInOut
+    })
+
+    .to(".boundingelem", {
+        y: 0,
+        ease: Expo.easeInOut,
+        duration: 1.5,
+        delay: -.7,
+        stagger: .2,
+    })
+
+    .from(".home-bottom", {
+        y: '-10',
+        opacity: 0,
+        duration: 1.2,
+        delay: -.9,
+        ease: Expo.easeInOut
+    })
 }
 
 crsrfollower();
+firstPage()
